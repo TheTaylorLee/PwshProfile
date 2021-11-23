7abae5a0cf32481dfa7193fad31bc46219b91d3a
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2021-11-23 16:24:39 -0600

    updates

--- PS7/variations/20211123 Microsoft.PowerShell_profile.ps1
+++ PS7/variations/20211123 Microsoft.PowerShell_profile.ps1
@@ -25,22 +25,19 @@ function reset-colors {
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 function prompt {
-    $(
-        if ((Get-Module oh-my-posh) -and (Get-Module terminal-icons) -and (Get-Module posh-git)) {
-            Set-PoshPrompt blue-owl
-        }
-        else {
-            $location = Get-Location
-            Write-Host -NoNewline "$(HOSTNAME.EXE) "                  -ForegroundColor Green
-            Write-Host -NoNewline '~'                                 -ForegroundColor Yellow
-            Write-Host -NoNewline $(Get-Location).Path.Split('\')[-1] -ForegroundColor Cyan
-            Write-Host -NoNewline ">" -ForegroundColor Green
-            $Adminp = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-            $ver = [string]$host.Version.major + '.' + [string]$host.version.minor + '.' + [string]$host.version.build + "-" + [string]$host.version.PSSemVerPreReleaseLabel
-            $host.UI.RawUI.WindowTitle = "$ver" + ' - Admin is ' + "$Adminp" + " - $location"
-            Return " "
-        }
-    )
+    reset-colors
+
+    $location = Get-Location
+    Write-Host -NoNewline "$(HOSTNAME.EXE) "                  -ForegroundColor Green
+    Write-Host -NoNewline '~'                                 -ForegroundColor Yellow
+    Write-Host -NoNewline $(Get-Location).Path.Split('\')[-1] -ForegroundColor Cyan
+    Write-Host -NoNewline ">" -ForegroundColor Green
+
+    $Adminp = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+    $ver = [string]$host.Version.major + '.' + [string]$host.version.minor + '.' + [string]$host.version.build + "-" + [string]$host.version.PSSemVerPreReleaseLabel
+    $host.UI.RawUI.WindowTitle = "$ver" + ' - Admin is ' + "$Adminp" + " - $location"
+
+    Return " "
 }
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -238,12 +235,6 @@ if ($AnsiRendering) {
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-try {
-    Import-Module terminal-icons
-    Import-Module posh-git
-    Import-Module oh-my-posh
-}
-catch {}
 Import-Module AdminToolbox
 Import-Module BetterCredentials
 Import-Module MyFunctions
---
+++ WindowsTerminal/setup.md
@@ -0,0 +1,35 @@
+[themes](https://ohmyposh.dev/docs/themes)
+[Fonts](https://www.nerdfonts.com/)
+
+# Terminal Setup
+- Install Terminal
+- Install nerd font of choice
+- Customize Terminal settings to desire and select nerd font of choice in the terminal
+- If using Oh-My-Posh be sure to include psportable profiles or know how to set your own prompt.
+
+# install modules once
+- install-module terminal-icons
+- install-module posh-git
+- install-module oh-my-posh
+
+# Prompt example
+```powershell
+function prompt {
+    $(
+        if ((Get-Module oh-my-posh) -and (Get-Module terminal-icons) -and (Get-Module posh-git)) {
+            Set-PoshPrompt blue-owl #tonybaloney, craver, atomic
+        }
+        else {
+            $location = Get-Location
+            Write-Host -NoNewline "$(HOSTNAME.EXE) "                  -ForegroundColor Green
+            Write-Host -NoNewline '~'                                 -ForegroundColor Yellow
+            Write-Host -NoNewline $(Get-Location).Path.Split('\')[-1] -ForegroundColor Cyan
+            Write-Host -NoNewline ">" -ForegroundColor Green
+            $Adminp = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+            $ver = [string]$host.Version.major + '.' + [string]$host.version.minor + '.' + [string]$host.version.build + "-" + [string]$host.version.PSSemVerPreReleaseLabel
+            $host.UI.RawUI.WindowTitle = "$ver" + ' - Admin is ' + "$Adminp" + " - $location"
+            Return " "
+        }
+    )
+}
+```
