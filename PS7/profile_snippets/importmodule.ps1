4378b9ba317b544871780aea555eea8f8aaadde3
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-01-29 22:58:42 -0600

    k

--- PS7/profile_snippets/importmodule.ps1
+++ PS7/profile_snippets/importmodule.ps1
@@ -1,8 +1,12 @@
 Import-Module AdminToolbox
 Import-Module BetterCredentials
 Import-Module MyFunctions
-Import-Module posh-git
-Import-Module oh-my-posh
+
+if ($Global:sow -eq '1') {
+    Import-Module posh-git
+    Import-Module oh-my-posh
+}
+
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
