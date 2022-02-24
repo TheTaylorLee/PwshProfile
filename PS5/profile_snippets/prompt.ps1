6cd167a40688be015eab8d3112939a1a85158403
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-02-23 21:57:48 -0600

    k

--- PS5/profile_snippets/prompt.ps1
+++ PS5/profile_snippets/prompt.ps1
@@ -6,7 +6,7 @@ function prompt {
     Write-Host -NoNewline ">" -ForegroundColor Green
 
     $Adminp = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-    $ver = [string]$host.Version.major + '.' + [string]$host.version.minor + '.' + [string]$host.version.build + "-" + [string]$host.version.PSSemVerPreReleaseLabel
+    $ver = [string]$host.Version.major + '.' + [string]$host.version.minor
     $host.UI.RawUI.WindowTitle = "$ver" + ' - Admin is ' + "$Adminp" + " - $location"
 
     Return " "
