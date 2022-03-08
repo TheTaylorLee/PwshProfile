c3d8c636610e59e7a65e3b832dff3320af7fbbf0
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-03-07 22:52:13 -0600

    Update Start-WindowsTerminal.bat

--- WindowsTerminal/ScheduledTask/Start-WindowsTerminal.bat
+++ WindowsTerminal/ScheduledTask/Start-WindowsTerminal.bat
@@ -1,4 +1,5 @@
 @echo off
+rem NEWER VERSIONS OF TERMINAL DON'T REQUIRE THIS METHOD TO LAUNCH AS ADMIN ON STARTUP
 rem This batch file allows for launching windows terminal with admin rights on login.
 rem Create a scheduled task to run this batch file on login.
 rem Be sure to specify this to run only for specific user
