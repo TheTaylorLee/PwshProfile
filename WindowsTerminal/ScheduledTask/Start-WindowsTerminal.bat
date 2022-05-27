1a2d239037ac47d8926196b9d1fcb199ed0f3d75
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-05-26 22:12:45 -0500

    Delete WindowsTerminal/ScheduledTask directory

--- WindowsTerminal/ScheduledTask/Start-WindowsTerminal.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem NEWER VERSIONS OF TERMINAL DON'T REQUIRE THIS METHOD TO LAUNCH AS ADMIN ON STARTUP
-rem This batch file allows for launching windows terminal with admin rights on login.
-rem Create a scheduled task to run this batch file on login.
-rem Be sure to specify this to run only for specific user
-rem Use own login and configure the task to run with highest privleges
-wt.exe
