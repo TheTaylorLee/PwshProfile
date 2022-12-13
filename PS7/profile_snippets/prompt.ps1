d807255e92e5a2bf5ffc96feed56653e09a73e7b
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-12-12 23:30:24 -0600

    prompt

--- PS7/profile_snippets/prompt.ps1
+++ PS7/profile_snippets/prompt.ps1
@@ -1,10 +1,10 @@
 function prompt {
     $(
-        if ((Get-Module oh-my-posh) -and (Get-Module posh-git) -and $Global:sow -eq '1') {
-            try { 
-                Set-PoshPrompt sonicboom_light
+        if ((Test-Path $env:userprofile\appdata\local\Programs\oh-my-posh\bin\oh-my-posh.exe) -and (Get-Module posh-git) -and $Global:sow -eq '1') {
+            try {
+                oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\sonicboom_light.omp.json | Invoke-Expression
             }
-            catch {  
+            catch {
                 $location = Get-Location
                 Write-Host -NoNewline "$(HOSTNAME.EXE) "                  -ForegroundColor Green
                 Write-Host -NoNewline '~'                                 -ForegroundColor Yellow
@@ -13,7 +13,7 @@ function prompt {
                 $Adminp = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
                 $ver = [string]$host.Version.major + '.' + [string]$host.version.minor + '.' + [string]$host.version.build + "-" + [string]$host.version.PSSemVerPreReleaseLabel
                 $host.UI.RawUI.WindowTitle = "$ver" + ' - Admin is ' + "$Adminp" + " - $location"
-                Return " " 
+                Return " "
             }
         }
         else {
